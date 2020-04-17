98cf157b3a5b29b9fd2a5e2d5ba9aa5254557781
henriettepilegaard <201507843@post.au.dk>
2020-04-17 16:49:30 +0200

    Setup of dummy date db

---
+++ Assignment2/Models/CheckIns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2.Models
+{
+    public class CheckIns
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public int Adults { get; set; }
+
+        public int Kids { get; set; }
+
+    }
+}
